54bd58e4a96a0bf08903b6f332d4e246da8dc657
Sebastian Laclé <info@slacle.com>
2022-11-06 23:44:05 -0600

    Did styling songs page.

--- components/layout/SongListItem.js
+++ components/layout/SongListItem.js
@@ -3,35 +3,31 @@ import Link from "next/link";
 export default function SongListItem({ song }) {
   return (
     <>
-      <Link href={"/" + song.slug}>
-        <div className="border mb-5 flex justify-between">
-          <div className="none">
-            <h3 className="none">{song.title}</h3>
-            <p className="none">{song.genre}</p>
-            <p className="none">{song.location}</p>
-          </div>
-          <div className="none">
-            <div className="none">
-              <span>{song.daw}</span>
-            </div>
-            {song.guitar && (
-              <div className="none">
-                <span>Guitar</span>
-              </div>
-            )}
-            {song.sample && (
-              <div className="none">
-                <span>Sample</span>
-              </div>
-            )}
-          </div>
-          <div className="none">
-            {song.plugins.length
-              ? song.plugins.map((p) => <span key={p}>{p}</span>)
-              : "No plugins used"}
-          </div>
-        </div>
-      </Link>
+      <tr className="border-b border-neutral-800">
+        <td className="px-5 py-5 bg-neutral-900 text-base font-semibold">
+          <Link
+            href={"/" + song.slug}
+            className="underline decoration-sky-400 decoration-0 hover:decoration-4"
+          >
+            <p className="text-neutral-300 whitespace-no-wrap">{song.title}</p>
+          </Link>
+        </td>
+        <td className="px-5 py-5 bg-neutral-900 text-sm">
+          <p className="text-neutral-300 whitespace-no-wrap">{song.genre}</p>
+        </td>
+        <td className="px-5 py-5 bg-neutral-900 text-sm hidden md:table-cell">
+          <p className="text-neutral-300 whitespace-no-wrap">{song.location}</p>
+        </td>
+        <td className="px-5 py-5 bg-neutral-900 text-sm hidden sm:table-cell">
+          <span className="relative inline-block px-3 py-1 font-semibold text-neutral-300 leading-tight">
+            <span
+              aria-hidden
+              className="absolute inset-0 bg-neutral-300 opacity-10 rounded-full"
+            ></span>
+            <span className="relative">{song.daw}</span>
+          </span>
+        </td>
+      </tr>
     </>
   );
 }
--- pages/songs.js
+++ pages/songs.js
@@ -4,10 +4,31 @@ import SongListItem from "../components/layout/SongListItem";
 export default function Songs() {
   return (
     <>
-      <h1 className="text-2xl font-bold pb-1 mb-5">Songs</h1>
-      {songs.map((song) => (
-        <SongListItem key={song.audius_id} song={song} />
-      ))}
+      <div className="inline-block min-w-full rounded-lg overflow-hidden my-5">
+        <table className="min-w-full leading-normal text-center">
+          <thead>
+            <tr>
+              <th className="px-5 py-3 bg-neutral-800 text-neutral-200 text-xs font-semibold uppercase tracking-wider">
+                Song
+              </th>
+              <th className="px-5 py-3 bg-neutral-800 text-neutral-200 text-xs font-semibold uppercase tracking-wider">
+                Genre
+              </th>
+              <th className="px-5 py-3 bg-neutral-800 text-neutral-200 text-xs font-semibold uppercase tracking-wider hidden md:table-cell">
+                Location
+              </th>
+              <th className="px-5 py-3 bg-neutral-800 text-neutral-200 text-xs font-semibold uppercase tracking-wider hidden sm:table-cell">
+                Daw
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {songs.map((song) => (
+              <SongListItem key={song.audius_id} song={song} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
