96fc6a36f2147a8e1e836556438720e4d2d0d694
Sebastian Laclé <info@slacle.com>
2022-11-08 02:23:30 -0600

    Add text to About page.

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,42 @@
 export default function About() {
   return (
-    <>
-      <h1 className="text-2xl font-bold pb-1">About</h1>
-      <div>All about sevaztian coming soon...</div>
-    </>
+    <div className="p-5 lg:p-10">
+      <h1 className="text-2xl font-bold pb-10">About</h1>
+      <p className="mb-8">
+        The idea was to make one short song a week while I taught myself music
+        production and learned to use the software for it. I stopped producing
+        music for various reasons, but the idea and wish has always been to pick
+        it back up again at some point.
+      </p>
+
+      <p className="mb-8">
+        I was inspired by Beeple over 11 years ago to start this project when I
+        saw that he was doing a sketch a day. Now he has inspired me again to
+        explore NFTs and am considering making NFTs of my tracks.
+      </p>
+
+      <p className="mb-8">
+        The images of the tracks were taken by myself. I like photography also
+        and eventually decided to start picking my favorite picture of the week
+        and post it online. You can see them in full size at{" "}
+        <a
+          href="https://www.xevaz.com/"
+          className="underline text-red-400 hover:text-red-500"
+        >
+          xevaz.com
+        </a>
+      </p>
+
+      <p className="mb-8">
+        I made, and am still working on this website by myself. I do{" "}
+        <a
+          href="https://slacle.com/"
+          className="underline text-green-300 hover:text-green-400"
+        >
+          web development
+        </a>{" "}
+        for a living.
+      </p>
+    </div>
   );
 }
