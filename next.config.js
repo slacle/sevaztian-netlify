f92cd7eeee110d545cb60fe2c8f1e04b41de9336
slacle <info@slacle.com>
2025-02-16 19:05:40 -0400

    Update next 13 to next 15

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     unoptimized: true,
     domains: ["https://creatornode.audius.co/"],
