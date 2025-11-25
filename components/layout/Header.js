6f77c3a928db1f3ff0e59a9b952808ba6e7dd38b
slacle <info@slacle.com>
2025-11-25 08:01:03 -0400

    change header and footer

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="text-neutral-200 bg-neutral-800 body-font">
-      <div className="container mx-auto flex flex-wrap xl:px-20 lg:px-10 px-5 py-5 flex-col sm:flex-row items-center">
+      <div className="container flex justify-center mx-auto xl:px-20 lg:px-10 px-5 py-5">
         <Link
           href="/"
-          className="flex title-font font-medium items-center text-white mb-4 sm:mb-0"
+          className="flex max-w-max title-font font-medium text-white mb-4 sm:mb-0"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -17,14 +17,6 @@ const Header = () => {
           </svg>
           <span className="ml-1 text-xl">sevaztian</span>
         </Link>
-        <nav className="sm:ml-auto flex flex-wrap items-center text-base justify-center">
-          <Link href="/songs" className="mr-10 hover:text-white">
-            Songs
-          </Link>
-          <Link href="/about" className="hover:text-white">
-            About
-          </Link>
-        </nav>
       </div>
     </header>
   );
